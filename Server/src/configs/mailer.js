c2b3693d5e0030357faf78a192f3bde217628363
Prayskey <prayskeyo@gmail.com>
2026-06-24 03:08:08 +0100

    Configure Nodemailer to allow unauthorized TLS connections in development

--- Server/src/configs/mailer.js
+++ Server/src/configs/mailer.js
@@ -1,14 +1,16 @@
-import nodemailer from 'nodemailer'
-import { configDotenv } from 'dotenv'
+import nodemailer from 'nodemailer';
+import { configDotenv } from 'dotenv';
+
 configDotenv();
+
+const isDev = process.env.NODE_ENV === 'development';
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
-  host: '://gmail.com',
-  port: 465,
-  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASS
-  }
+  },
+  // Automatically apply the bypass ONLY in development mode
+  tls: isDev ? { rejectUnauthorized: false } : undefined
 });
