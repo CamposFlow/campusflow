6b28694343803e52932eaa6609d702ab5de27a2d
Prayskey <prayskeyo@gmail.com>
2026-06-26 15:18:11 +0100

    Update codebase

--- Server/src/controllers/auth.controllers.js
+++ Server/src/controllers/auth.controllers.js
@@ -18,8 +18,8 @@ export const register = async (req, res, next) => {
   const { fullname, email, role, university, password } = req.body;
   console.log(req.body);
 
-  
-  const testCases = [ fullname, email, role, university, password ];
+
+  const testCases = [fullname, email, role, university, password];
 
   testCases.forEach(e => {
     if (!e) console.log(e + " is not defined.")
@@ -44,7 +44,7 @@ export const register = async (req, res, next) => {
     res.status(201).json({
       message: 'User registered successfully',
       token: `Bearer ${token}`,
-      user: { id: newUser.id, fullname: newUser.fullname, email: newUser.email, role: newUser.role, university: newUser.university }
+      user: { id: newUser.id, fullname: newUser.fullname, email: newUser.email, role: newUser.role }
     });
   } catch (err) {
     console.error('Registration error:', err);
