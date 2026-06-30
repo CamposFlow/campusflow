eade57e4d94f650be75611b05a6799680160b9fa
Eziokwubundu Jenissi Ezichi <eziokwubundujenissi@gmail.com>
2026-06-30 14:48:40 +0100

    Client

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "https://campusflowserver.onrender.com",
+    baseURL: "https://campusflowserver-uc79.vercel.app",
     headers: { 'Content-Type': 'application/json' }
 });
 
