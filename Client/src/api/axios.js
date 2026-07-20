ad677945eed0fdf5f4e14cea18c6021100cfba79
Eziokwubundu Jenissi Ezichi <eziokwubundujenissi@gmail.com>
2026-07-20 22:23:58 +0100

    d

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -8,8 +8,6 @@ const api = axios.create({
 });
 
 
-
-
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token")|| sessionStorage.getItem("token");
     if (token) {
