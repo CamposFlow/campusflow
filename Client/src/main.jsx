e95395ee0a39131377427ae9e621c4a2d9f71ee3
Eziokwubundu Jenissi Ezichi <eziokwubundujenissi@gmail.com>
2026-07-21 07:22:05 +0100

    d

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { AuthProvider } from "./pages/AuthContext.jsx";
 import App from "./App.jsx";
@@ -12,11 +13,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 createRoot(document.getElementById("root")).render(
   <StrictMode>
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
       </GoogleOAuthProvider>
   </StrictMode>,
 );
